e5619649f443a443fd76b85e8813e4cd0de48b61
Dinh Tri <tri.vodinh.53@gmail.com>
2021-01-18 21:53:39 +0700

    +update

--- TestVoice/main.98bce.js
+++ TestVoice/main.98bce.js
@@ -118,7 +118,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.db255.js');
+        require('src/settings.2c420.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -126,7 +126,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.db255.js');
+        require('src/settings.2c420.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
