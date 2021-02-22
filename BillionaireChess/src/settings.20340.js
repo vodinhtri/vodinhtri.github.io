a229e373169477e205ec5d116eec3eb91812901b
Dinh Tri <tri.vodinh.53@gmail.com>
2021-02-23 01:01:24 +0700

    +update

--- BillionaireChess/src/settings.20340.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/game.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",main:"fc830"}};
