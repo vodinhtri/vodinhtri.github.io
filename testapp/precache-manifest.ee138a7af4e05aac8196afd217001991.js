84a2f22ba0554bdb54f7771dbc777d430e8aae99
Dinh Tri <tri.vodinh.53@gmail.com>
2021-07-15 09:35:28 +0700

    +update

---
+++ testapp/precache-manifest.ee138a7af4e05aac8196afd217001991.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1637be49b76c9dcc7404a3936cb1ef2d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a44414a8d122605bd655",
+    "url": "./static/css/2.186ebe94.chunk.css"
+  },
+  {
+    "revision": "3fb40a19b5444cd4dd1b",
+    "url": "./static/css/main.6f6d1a82.chunk.css"
+  },
+  {
+    "revision": "a44414a8d122605bd655",
+    "url": "./static/js/2.50d04096.chunk.js"
+  },
+  {
+    "revision": "3fb40a19b5444cd4dd1b",
+    "url": "./static/js/main.3aa74755.chunk.js"
+  },
+  {
+    "revision": "9541b778ee10a5f82c98",
+    "url": "./static/js/runtime-main.3697a557.js"
+  },
+  {
+    "revision": "47f711bd1524b9283aa92d3d74db16a8",
+    "url": "./static/media/BebasNeue-Regular.47f711bd.ttf"
+  },
+  {
+    "revision": "9ab0544da4ed08ba58b22d235baf57ce",
+    "url": "./static/media/BebasNeue-Regular.9ab0544d.woff2"
+  },
+  {
+    "revision": "afeaf6797b6286b72b613ababc563807",
+    "url": "./static/media/BebasNeue-Regular.afeaf679.woff"
+  },
+  {
+    "revision": "d4f210f483d46c9371572e3fcd0f1d4a",
+    "url": "./static/media/BebasNeue-Regular.d4f210f4.eot"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "./static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "./static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "./static/media/revicons.2feb69cc.eot"
+  }
+]);
